f06bd9565fc1c1c214678aa734d57cf5e1e1c7d8
Ajinkya <asbadve@gmail.com>
2016-01-30 09:54:06 +0530

    minor changes done

---
+++ fabmenu/src/main/java/com/example/gobol/fabmenu/view/FabButton.java
@@ -0,0 +1,19 @@
+package com.example.gobol.fabmenu.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.design.widget.FloatingActionButton;
+
+/**
+ * Created by Ajinkya on 1/25/2016.
+ */
+public class FabButton extends FloatingActionButton {
+    public FabButton(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
